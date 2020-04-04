a8e57169aff77dd8ac55d23244e5a406ace72260
PatrickHirsch <45246344+PatrickHirsch@users.noreply.github.com>
2020-04-04 17:30:12 -0400

    Add files via upload

---
+++ school/2020Sp/MashComm36/WarTrack/script.js
@@ -0,0 +1,8 @@
+function demoExport()
+{	var string = "Idealy, this would export a PDF of Bob's Warior Traker enteries from past grades";
+	alert(string);
+}
+function demoSave()
+{	var string = "Idealy, this would save Bob's data to the Warior Traker database until he comesback to edit it later.";
+	alert(string);
+}
